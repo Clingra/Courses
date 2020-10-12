e7fda568ee6d427da96056762228060f07eb2661
clingra <curtis.lee.ingram@gmail.com>
2020-10-12 02:23:24 -0500

    Program start

---
+++ HW4Computation.py
@@ -0,0 +1,11 @@
+#Curtis Ingram, ID 1388990
+#Assignment 4 - Computing Problem
+#goal of this assignment is to solve the Helmholtz 
+#equation with the tri-diagonal algorithm
+import math
+
+L=None #interval length
+x=None #internal points
+h=None #increment h=L/(N+1)
+N=10
+
