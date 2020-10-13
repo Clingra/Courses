4d7a10dbdaaf4587f57f96a825f082a860860ee7
clingra <curtis.lee.ingram@gmail.com>
2020-10-13 09:52:34 -0500

    third attempt at a solution for the helmholtz equation

--- HW4Computation2.py
+++ HW4Computation2.py
@@ -1,21 +1,21 @@
 import numpy
-#def tridiag(a,b,c,f):
-N=10
-h=1/N
-a=-2*numpy.ones([N,1])
-b=(1-h^2)*numpy.ones([N,1])
-c=numpy.ones([N,1])
-f=h^2*numpy.ones([N,1])+(h^2-1)*numpy.identity([N,1])
-l=len(f)
-v=numpy.zeros([n,1])
-y=v
-w=a(1)
-y(1)=f(1)/w
-for i in [list(range(2,n))]:
-    v(i-1)=c(i-1)/w
-    w=a(i)-b(i)*v(i-1)
-    y(i)=(f(i)-b(i)*y(i-1))/w
-for j in [list(range(n-1,1))]:
-    y(j)=y(j)-v(j)*y(j+1)
-    j=j-1
-return
+def tridiag(a,b,c,d):
+    N=10
+    h=1/N
+    a=-2*numpy.ones([N,1])
+    b=(1-h^2)*numpy.ones([N,1])
+    c=numpy.ones([N,1])
+    d=h^2*numpy.ones([N,1])+(h^2-1)*numpy.identity([N,1])
+    l=len(d)
+    v=numpy.zeros([n,1])
+    y=v
+    w=a(1)
+    y(1)=d(1)/w
+    for i in [list(range(2,n))]:
+        v(i-1)=c(i-1)/w
+        w=a(i)-b(i)*v(i-1)
+        y(i)=(f(i)-b(i)*y(i-1))/w
+    for j in [list(range(n-1,1))]:
+        y(j)=y(j)-v(j)*y(j+1)
+        j=j-1
+    return y
---
+++ HW4Computation3.py
@@ -0,0 +1,3 @@
+#The purpose of this code is to use the tri-diagonal (Thomas) algorithm
+#to solve the Helmholtz equation using N equally-spaced points
+import numpy
